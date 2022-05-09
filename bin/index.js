3ae52eb8e928d7d67b3632070e1e45ed9c8fa7c2
Leif Arriens <leif.arriens@gmail.com>
2022-05-09 16:21:24 +0200

    added folder support

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const { program } = require('commander');
 const sharp = require('sharp');
 const fs = require('fs');
+const path = require('path');
 
 program
   .command('convert <img>')
@@ -12,11 +13,47 @@ program
 
 program.parse();
 
-async function convert(img, options) {
+async function convert(inputPath, options) {
   const { format = 'jpeg' } = options;
-  const file = fs.readFileSync(img);
+  const stats = fs.statSync(inputPath);
+  const isDir = stats.isDirectory();
 
-  await sharp(file)
-    .toFormat(format)
-    .toFile(`${img.split('.')[0]}.${format}`);
+  if (isDir) {
+    const files = fs.readdirSync(inputPath).map((fileName) => {
+      const filePath = path.resolve(inputPath, fileName);
+      const outPath = path.resolve(
+        inputPath.replace(fileName, ''),
+        '../',
+        `${path.parse(fileName).name}.${format}`
+      );
+      return {
+        name: fileName,
+        folder: inputPath,
+        path: filePath,
+        file: fs.readFileSync(filePath),
+        outPath,
+      };
+    });
+
+    const transforms = files.map((file) => {
+      console.log(file.outPath);
+      return convertImage(file.path, {
+        format,
+        outPath: file.outPath,
+      });
+    });
+
+    return await Promise.all(transforms);
+  }
+
+  const fileBuffer = fs.readFileSync(path.resolve(inputPath));
+
+  await convertImage(fileBuffer, {
+    format,
+    outPath: inputPath.replace(path.extname(inputPath), '') + `.${format}`,
+  });
+}
+
+async function convertImage(fileBuffer, { format, outPath }) {
+  return sharp(fileBuffer).toFormat(format).toFile(outPath);
 }
